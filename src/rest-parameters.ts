dbaf2b9275fd685840f00225ed2ec21a64f9bcef
Hayato Kakiuchi <qweaty.uiop8810@i.softbank.jp>
2020-02-29 19:38:00 +0900

    done rest params

---
+++ src/rest-parameters.ts
@@ -0,0 +1,14 @@
+export {};
+
+const reducer: (accumulator: number, currentValue: number) => number = (
+  accumulator: number,
+  currentValue: number
+): number => {
+  return accumulator + currentValue;
+};
+
+const sum: (...values: number[]) => number = (...values: number[]): number => {
+  return values.reduce(reducer);
+};
+
+console.log(sum(1, 2, 3, 4));
