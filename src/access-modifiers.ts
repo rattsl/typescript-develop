18f74b085443c6ac04dc799e7c30dc1153091422
Hayato Kakiuchi <qweaty.uiop8810@i.softbank.jp>
2020-03-01 15:37:06 +0900

    fix access-modifiers

--- src/access-modifiers.ts
+++ src/access-modifiers.ts
@@ -3,8 +3,10 @@ export {};
 //クラスは大文字
 class Person {
   //propatyの型の宣言
+  //↓メンバ変数
   public name: string;
-  private age: number;
+  //private age: number;
+  protected age: number;
   protected nationarity: string;
 
   //constructorはreturnしないのでアノテーション不要
@@ -15,11 +17,28 @@ class Person {
   }
 
   profile(): string {
-    return `im ${this.name} and ${this.age} years old and im from ${this.nationarity}! `;
+    return `im ${this.name} and ${this.age} years old! `;
+  }
+}
+
+//subclass作成
+class Android extends Person {
+  constructor(name: string, age: number, nationarity: string) {
+    //superを呼ぶことで親クラスのconstructorメソッドを呼ぶことができる
+    //親クラスのコンストラクタメソッドで期待されている引数を渡す
+    super(name, age, nationarity);
+  }
+
+  profile(): string {
+    return `im ${this.name} and ${this.age}years old, and from ${this.nationarity}!`;
   }
 }
 
 //taroインスタンスを生成
 let taro = new Person('taro', 13);
+let hanakoBot = new Android('hanako', 1, 'ibm');
 console.log(taro.profile());
+console.log(hanakoBot.profile());
 // console.log(taro.age)
+
+//クラスを人に使ってもらう人(npm packageを開発する人など)はアクセス修飾子を意識する必要がある
